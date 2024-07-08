677cb08f5bc06b04af17a516539c62a8084d7a33
giorgizankaidze <giorgi.zankaidze@optio.ai>
2024-07-08 17:52:07 +0400

    feat: install typeorm

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,7 +9,12 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService, {
+        provide: UsersService,
+        useValue: {
+          findAll: jest.fn().mockResolvedValue(['user1', 'user2']),
+        },
+      },],
     }).compile();
 
     appController = app.get<AppController>(AppController);
@@ -17,6 +23,13 @@ describe('AppController', () => {
   describe('root', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
+
+  describe('users', () => {
+    it('should return an array of users', async () => {
+      const users = await appController.getAllUsers();
+      expect(users).toEqual(['user1', 'user2']);
     });
   });
 });
