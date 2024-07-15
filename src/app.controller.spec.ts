2afb0219e5c7a163e346357842001de13791f671
giorgizankaidze <giorgi.zankaidze@optio.ai>
2024-07-15 21:26:26 +0400

    feat: add prisma migrations

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -20,9 +20,5 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
-  });
+  describe('root', () => {});
 });
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,26 @@
-export class Product {}
+export class Product {
+    import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false })
+  name: string;
+
+  @Column({ nullable: false, type: 'text' })
+  description: string;
+
+  @Column({ nullable: false, type: 'decimal', precision: 10, scale: 2 })
+  price: number;
+
+  @Column({ nullable: false, default: 0 })
+  stockQuantity: number;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+}
