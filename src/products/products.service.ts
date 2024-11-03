a723855a16d4ccbbe55ecacd9cac5550b9a3dc79
giorgizanqaidze <giorgizanqaidzeofficial@gmail.com>
2024-11-04 03:03:38 +0400

    refactor: products module

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,12 +1,15 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, InternalServerErrorException, NotFoundException } from "@nestjs/common";
 import { ProductDto } from "./dto/product.dto";
 import { UpdateProductDto } from "./dto/update-product.dto";
 import { PrismaService } from "../prisma/prisma.service";
 import { Product } from "@prisma/client";
+import { CustomLoggerService } from "src/common";
 
 @Injectable()
 export class ProductsService {
   constructor(private prisma: PrismaService) {}
+
+  logger = new CustomLoggerService(ProductsService.name);
 
   async create(createProductDto: ProductDto) {
     return this.prisma.product.create({
@@ -19,9 +22,21 @@ export class ProductsService {
   }
 
   async findOne(id: number) {
-    return this.prisma.product.findUnique({
-      where: { id },
-    });
+    try {
+      const product = await this.prisma.product.findUnique({
+        where: { id: Number(id) },
+      });
+
+      if (!product) {
+        throw new NotFoundException(`Product with ID ${id} not found.`);
+      }
+
+      return product;
+    } catch (error) {
+      // Handle Prisma errors or other unexpected errors
+      this.logger.error(error);
+      throw new InternalServerErrorException("An error occurred while retrieving the product.");
+    }
   }
 
   async update(id: number, updateProductDto: UpdateProductDto) {
